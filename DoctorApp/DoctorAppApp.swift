0e962254ad106a09ecfa15bae85ec3113a173509
LeRusseBesuhof <p.grad0ff@gmail.com>
2024-08-17 15:23:08 +0700

    Initial Commit

---
+++ DoctorApp/DoctorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoctorAppApp.swift
+//  DoctorApp
+//
+//  Created by Павел Градов on 17.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct DoctorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
